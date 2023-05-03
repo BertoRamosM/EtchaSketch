6e6ff8be9c51ee57241ff6e2e532a6f0fa30dd36
AlbertoLaptop <albertramos902@gmail.com>
2023-05-03 02:40:03 +0200

    re-write code for better

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-const gridContainer = document.querySelector(".gridContainer")
+const gridContainer = document.querySelector('.gridContainer');
+const clear = document.querySelector('.clear');
+const gridSizeRange = document.querySelector('.gridSize');
+const color = document.querySelector('.color');
+const rainbow = document.querySelector('.rainbow');
+const gridCells =  document.querySelectorAll('.gridCells');
 
-const cells = document.querySelectorAll('.gridCells');
- 
-//grid creation
+
+
+
 function createGrid(){
-    gridContainer.innerHTML = "";
+    
+    gridSizeRange.addEventListener('input', event=>{
+            
+        let gridSize = 16;
+        gridSize = event.target.value;
+        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+        gridContainer.innerHTML="";
+
+        for(let i=0; i < gridSize * gridSize; i++) {
+            const grid = document.createElement('div');
+            grid.classList.add('gridCells');
+            gridContainer.appendChild(grid);
+            const logSize = document.querySelector('.logSize');
+        logSize.textContent = `${gridSize}x${gridSize}`;
+        
+        }
+        setEvents();
+        });
+        gridSizeRange.dispatchEvent(new Event('input'));
+       
+        
 }
+    
+        
+createGrid();
 
-let gridRow = 16;
-let gridCol = 16;
-
-
-
-
-gridContainer.style.gridTemplateColumns = `repeat(${gridCol}, 1fr)`;
-gridContainer.style.gridTemplateRows = `repeat (${gridRow}, 1fr)`;
+//store all functions added to the grid
+function setEvents(){
 
 
-for (let i = 0; i < gridCol * gridRow -1; i++) {
-    const gridCell = document.createElement("div");
-    gridCell.classList.add("gridCells");
-    gridContainer.appendChild(gridCell);
+//clear Grid
+function clearGrid(){
+    const gridCells = document.querySelectorAll('.gridCells');
+
+    clear.addEventListener('click', ()=>{
+        gridCells.forEach(gridCells =>{
+            gridCells.style.backgroundColor = 'black';
+        })
+    })
 }
+clearGrid();
 
 
 
 
-
-
-//paint cells individually
 //paint on click
 let onClick = false;
 
@@ -45,70 +72,58 @@ function paintClick(){
 
 paintClick();
 
-
+let chooseColor = 'white';
 
 //paint on drag
-function paintWhite(){
+function paintCell(){
     const cells = document.querySelectorAll('.gridCells');
     cells.forEach(cell =>{
         cell.addEventListener('mouseover', ()=>{
             if(onClick === true){
-            cell.style.backgroundColor = "white";
+            cell.style.backgroundColor = chooseColor;
             }
         });
     });
 }
 
-paintWhite();
+paintCell();
 
 
-//turn back a single cell to black
-function PaintCellBlack(){
-    const cells = document.querySelectorAll('.gridCells');
-    cells.forEach(cell =>{
-        cell.addEventListener('dblclick',()=>{
-            cell.style.backgroundColor = "black";
-        })
-    })
-}
-PaintCellBlack();
-
-
-//delete all color inserted, back to black
-function clearBoard(){
-
-    const cells = document.querySelectorAll('.gridCells');
-    const clearBtn = document.querySelector(".clear");
-
-    clearBtn.addEventListener('click', ()=>{
-        cells.forEach(cell=>{
-            cell.style.backgroundColor = "black";
-        })
-    })
-
-}
-clearBoard();
-
-//change size by using the input slide
-function ChangeSize(){
-    const size = document.querySelector('.gridSize');
-    size.addEventListener('input',()=>{
- 
-       gridCol = size.value;
-       gridRow = size.value;
-
-    gridContainer.style.gridTemplateColumns = `repeat(${gridCol}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat (${gridRow}, 1fr)`;
+//choose color
+function colorSelector(){
+    const color = document.querySelector('.colorSel');
+    color.addEventListener('input', event =>{
+        chooseColor = event.target.value
     })
 }
 
-ChangeSize();
+colorSelector();
 
 
 
-function rainBow(){
-    const random = document.querySelector('.rainbow');
-    random.addEventListener('click', ()=>{
-        
-    })
+
+
+let randColor = () => {
+    let hexColor = Math.floor(Math.random() * 16777213).toString(16);
+    return `#${hexColor}`;
 }
+   
+randColor();
+
+function getRainbow(){
+    const rainbowColor = document.querySelector('.rainbow');
+    rainbowColor.addEventListener('click',()=>{
+    chooseColor = randColor;
+})}
+
+getRainbow();
+
+
+
+
+}
+
+
+
+
+setEvents();
